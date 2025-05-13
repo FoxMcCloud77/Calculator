499ed2acd3f92705b742fce27e70989ad8028255
Denis Fomin <Denis2000Fomin@gmail.com>
2025-05-13 11:57:49 -0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Denis Fomin on 5/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello, world!")
+    }
+
+
+}
+
